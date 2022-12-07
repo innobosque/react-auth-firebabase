94853156d22507f5a847fb44d6c1198c878084ca
Xurxo González <xgonten.innobosque.gmail.com>
2022-12-07 16:58:51 +0100

    Añadido botón de reseteo

--- src/components/BtnGroup.jsx
+++ src/components/BtnGroup.jsx
@@ -1,15 +1,15 @@
 import { useForm } from "../"
 
 export const BtnGroup = ({ userBool, loading, handleSignInWithEmailPassword, handleSignOut }) => {
-    const {handleInputChange,email,password} = useForm({
-        email: '',
-        password: '',
+    const {handleResetForm,handleInputChange,email,password} = useForm({
+        email: 'homegoma@hotmail.com',
+        password: '123456',
     });
     return (
         <div className="btn-group">
             {
             !userBool?
-            <div>
+            <div className="text-center">
                 <input
                 className="block w-full my-2 px-2 py-1 rounded" 
                 name="email"
@@ -28,6 +28,11 @@ export const BtnGroup = ({ userBool, loading, handleSignInWithEmailPassword, han
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
                     style={{ backgroundColor: loading ? 'rgb(248,113,113)' : '' }}>
                     {loading ? <span role="alert">Cargando...</span> : 'Acceso'}
+                </button>
+                <button
+                onClick={handleResetForm}
+                className="bg-slate-700 hover:bg-slate-900 text-white font-bold ml-2 py-2 px-4 rounded">
+                    Reseteo
                 </button>
             </div>
             :
